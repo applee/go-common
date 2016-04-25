92fbb68782d6cb3655ae111780befdb78580177d
applee <applee@gmail.com>
2016-04-25 20:09:19 +0800

    DES加解密

---
+++ crypt/des.go
@@ -0,0 +1,32 @@
+package crypt
+
+import (
+	"crypto/cipher"
+	"crypto/des"
+
+	"github.com/applee/go-common/crypt/padding"
+)
+
+func DesEncrypt(origData, key []byte) ([]byte, error) {
+	block, err := des.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	origData = padding.PKCS5.Padding(origData, block.BlockSize())
+	blockMode := cipher.NewCBCEncrypter(block, key)
+	crypted := make([]byte, len(origData))
+	blockMode.CryptBlocks(crypted, origData)
+	return crypted, nil
+}
+
+func DesDecrypt(crypted, key []byte) ([]byte, error) {
+	block, err := des.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	blockMode := cipher.NewCBCDecrypter(block, key)
+	origData := make([]byte, len(crypted))
+	blockMode.CryptBlocks(origData, crypted)
+	origData, err = padding.PKCS5.Unpadding(origData, block.BlockSize())
+	return origData, err
+}
