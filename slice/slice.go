c2232456842a5ab6cc822f2a8f951ab979c8f962
Applee <applee@gmail.com>
2017-04-26 11:08:43 +0800

    update slice functions

--- slice/slice.go
+++ slice/slice.go
@@ -131,22 +131,13 @@ loop:
 	return d.Slice(0, m).Interface(), nil
 }
 
-// Contains return the whether the obj is in target
-func Contains(obj interface{}, target interface{}) (bool, error) {
-	targetValue := reflect.ValueOf(target)
-	switch reflect.TypeOf(target).Kind() {
-	case reflect.Slice, reflect.Array:
-		for i := 0; i < targetValue.Len(); i++ {
-			if targetValue.Index(i).Interface() == obj {
-				return true, nil
-			}
-		}
-	case reflect.Map:
-		if targetValue.MapIndex(reflect.ValueOf(obj)).IsValid() {
-			return true, nil
+func Contains(obj interface{}, target []interface{}) bool {
+	for _, v := range target {
+		if v == obj {
+			return true
 		}
 	}
-	return false, errors.New("not in")
+	return false
 }
 
 // ContainsInt returns wether the obj is in the target.
