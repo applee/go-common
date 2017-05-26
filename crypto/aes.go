eae45eaf6bb3e054df40c76ed84df192cae1628f
Applee <applee@gmail.com>
2017-05-26 16:27:33 +0800

    Recover panic in AES crypt/decrypt

--- crypto/aes.go
+++ crypto/aes.go
@@ -3,11 +3,17 @@ package crypto
 import (
 	"crypto/aes"
 	"crypto/cipher"
+	"fmt"
 
 	"github.com/applee/go-common/crypto/padding"
 )
 
-func AesEncrypt(origData, key []byte) ([]byte, error) {
+func AesEncrypt(origData, key []byte) (crypted []byte, err error) {
+	defer func() {
+		if e := recover(); e != nil {
+			err = fmt.Errorf("panic:%v", e)
+		}
+	}()
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
@@ -15,12 +21,17 @@ func AesEncrypt(origData, key []byte) ([]byte, error) {
 	blockSize := block.BlockSize()
 	origData = padding.PKCS7.Padding(origData, blockSize)
 	blockMode := cipher.NewCBCEncrypter(block, key[:blockSize])
-	crypted := make([]byte, len(origData))
+	crypted = make([]byte, len(origData))
 	blockMode.CryptBlocks(crypted, origData)
 	return crypted, nil
 }
 
-func AesDecrypt(crypted, key []byte) ([]byte, error) {
+func AesDecrypt(crypted, key []byte) (original []byte, err error) {
+	defer func() {
+		if e := recover(); e != nil {
+			err = fmt.Errorf("panic:%v", e)
+		}
+	}()
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
@@ -29,6 +40,6 @@ func AesDecrypt(crypted, key []byte) ([]byte, error) {
 	blockMode := cipher.NewCBCDecrypter(block, key[:blockSize])
 	origData := make([]byte, len(crypted))
 	blockMode.CryptBlocks(origData, crypted)
-	origData, _ = padding.PKCS7.Unpadding(origData, blockSize)
-	return origData, nil
+	original, _ = padding.PKCS7.Unpadding(origData, blockSize)
+	return original, nil
 }
