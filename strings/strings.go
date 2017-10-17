3156f00b642722cb81f2050ff57487ed8f27cc89
Applee <applee@gmail.com>
2017-10-17 11:11:28 +0800

    feat(strings): add func SubString and SubStr

--- strings/strings.go
+++ strings/strings.go
@@ -3,6 +3,7 @@ package strings
 
 import (
 	"reflect"
+	"unicode/utf8"
 	"unsafe"
 )
 
@@ -16,4 +17,50 @@ func FromBytes(b []byte) string {
 	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
 	sh := reflect.StringHeader{Data: bh.Data, Len: bh.Len}
 	return *(*string)(unsafe.Pointer(&sh))
+}
+
+func SubString(s string, start int, end int) string {
+	var i, j, idx int
+	if start > end {
+		return s
+	}
+	if start < 0 || end < 0 {
+		l := utf8.RuneCountInString(s)
+		if start < 0 {
+			start = l + start
+		}
+		if end < 0 {
+			end = l + end
+		}
+	}
+	for j = range s {
+		if i == start {
+			idx = j
+		}
+		if i == end && end > 0 {
+			return s[idx:j]
+		}
+		i++
+	}
+	return s[idx:]
+}
+
+func SubStr(s string, start int, length int) string {
+	var i, j, idx int
+	if length <= 0 {
+		return s
+	}
+	if start < 0 {
+		start = utf8.RuneCountInString(s) + start
+	}
+	for j = range s {
+		if i == start {
+			idx = j
+		}
+		if i == start+length {
+			return s[idx:j]
+		}
+		i++
+	}
+	return s[idx:]
 }
--- strings/strings_test.go
+++ strings/strings_test.go
@@ -23,3 +23,22 @@ func BenchmarkS2BFast(b *testing.B) {
 		b.Error("failed")
 	}
 }
+
+func TestSubString(t *testing.T) {
+	s := "你好hello中国"
+	cases := []struct {
+		start, end int
+		want       string
+	}{
+		{0, 2, "你好"},
+		{-2, -1, "中"},
+		{-1, 0, "国"},
+	}
+
+	for idx, c := range cases {
+		if got := SubString(s, c.start, c.end); got != c.want {
+			t.Fatalf("case %d: want: %s, got: %s", idx, c.want, got)
+			break
+		}
+	}
+}
