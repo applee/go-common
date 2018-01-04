0d974e33561d7c4889c758cb8e95ac29a41d4fe6
Applee <applee@gmail.com>
2018-01-04 11:16:20 +0800

    feat(fs): add ReadLines and WriteLines funcs

--- fs/fs.go
+++ fs/fs.go
@@ -1,6 +1,7 @@
 package fs
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"os"
@@ -78,4 +79,35 @@ func CopyContents(src, dest string) (err error) {
 	}
 	err = out.Sync()
 	return
+}
+
+// ReadLines reads the file lines to slice.
+func ReadLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+// WriteLines writes the lines to the specific file.
+func WriteLines(lines []string, path string) error {
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	w := bufio.NewWriter(file)
+	for _, line := range lines {
+		fmt.Fprintln(w, line)
+	}
+	return w.Flush()
 }
